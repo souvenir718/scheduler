92cceb017747a3e36c540aa72c76ba71348a6f4f
souvenir <mongkey0@naver.com>
2020-12-20 10:24:34 +0900

    [1220] 기본 세팅

---
+++ src/Scheduler/Scheduler.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Scheduler.scss";
+const Scheduler = () => {
+    return (
+        <div>
+            <span>Scheduler</span>
+        </div>
+    )
+}
+
+export default Scheduler
